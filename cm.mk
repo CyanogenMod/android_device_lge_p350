0c037df68b233cf1cc890f622e0093e8e2516933
Koushik Dutta <koushd@gmail.com>
2012-09-16 22:36:18 -0700

    cm.mk

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := p350
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/p350/device_p530.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := p350
+PRODUCT_NAME := cm_p350
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := p350
+PRODUCT_MANUFACTURER := lge
